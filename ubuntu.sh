281d6aabe99ff50896eec8de832477e5da5c8aba
konstruktoid <konstruktoid@users.noreply.github.com>
2015-04-25 17:09:29 +0200

    fix link and retab

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,7 +2,8 @@
 # konstruktoid.net
 #
 # Documentation:
-# CIS Ubuntu 14.04 LTS Server Benchmark
+# https://github.com/konstruktoid/hardening
+# https://benchmarks.cisecurity.org/downloads/show-single/?file=ubuntu1404.100
 # https://wiki.ubuntu.com/Security/Features
 # https://help.ubuntu.com/community/StricterDefaults
 #
@@ -10,26 +11,25 @@
 FW_ADMIN="192.168.1.71"
 SSH_GRPS="sudo"
 FW_POLICY="https://raw.githubusercontent.com/konstruktoid/ubuntu-conf/master/net/firewall"
-FW_SERVICE="./scripts/firewall.service"
+FW_SERVICE="https://raw.githubusercontent.com/konstruktoid/ubuntu-conf/hardening/config/firewall.service"
 SYSCTL_CONF="https://raw.githubusercontent.com/konstruktoid/ubuntu-conf/master/misc/sysctl.conf"
 AUDITD_RULES="https://raw.githubusercontent.com/konstruktoid/ubuntu-conf/master/misc/audit.rules"
 VERBOSE="Y"
-CHANGEME="ad"		# Add something just to verify that you actually glanced the code
+CHANGEME="ad" # Add something just to verify that you actually glanced the code
 
 clear
 
 if ! [ -x "`which systemctl`" ]
-	then
-  		echo "systemctl required. Exiting."
-		exit
+  then
+    echo "systemctl required. Exiting."
+    exit
 fi
 
 for s in `ls -1d ./scripts/[0-9_]*`; do
-	. $s
+  . $s
 done
 
 echo
-echo "!!  /etc/systemd/system.conf !!"
 
 pre
 firewall
