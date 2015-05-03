6e88959dabe62854b462a08808e88da128d9a42f
konstruktoid <konstruktoid@users.noreply.github.com>
2015-05-03 21:59:18 +0200

    reformat

--- config/initpath.sh
+++ config/initpath.sh
@@ -1,5 +1,6 @@
-if [ $EUID -eq 0 ] ; then
-  export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-    else
-  export PATH=/usr/local/bin:/usr/bin:/bin
+if [[ $EUID -eq 0 ]];
+  then
+    export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+  else
+    export PATH=/usr/local/bin:/usr/bin:/bin
 fi
