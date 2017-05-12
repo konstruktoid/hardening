1e7fcabf288274550086c8526efe575c77b37458
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2017-05-12 11:31:05 +0200

    add auditgenerator
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

---
+++ misc/auditgenerator.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+PRIFILE="$(mktemp)"
+echo "$PRIFILE"
+for x in $(mount | grep '/dev/mapper' | awk '{print $3}'); do
+  for f in $(sudo find "$x" -xdev -type f -perm -4000 -o -type f -perm -2000 2>/dev/null); do
+    if sudo stat -c %A "$f" | grep 'x' 2>/dev/null 1>&2; then
+       echo "-a always,exit -F path=$f -F perm=x -F auid>=1000 -F auid!=4294967295 -F key=privileged" >> "$PRIFILE"
+    fi
+  done
+done
+
+sort "$PRIFILE" | uniq
+
+rm "$PRIFILE"
