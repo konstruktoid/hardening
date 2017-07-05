1149d5a8798b2055ce102779e4a1f99661846990
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2017-07-05 11:06:41 +0200

    more general audit generation
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- misc/auditgenerator.sh
+++ misc/auditgenerator.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 PRIFILE="$(mktemp)"
 echo "$PRIFILE"
-for x in $(mount | grep '/dev/mapper' | awk '{print $3}'); do
+for x in $(lsblk  -n | awk '{print $NF}' | grep -E -v '[SWAP]|disk|part' | sort | uniq); do
   for f in $(sudo find "$x" -xdev -type f -perm -4000 -o -type f -perm -2000 2>/dev/null); do
     if sudo stat -c %A "$f" | grep 'x' 2>/dev/null 1>&2; then
        echo "-a always,exit -F path=$f -F perm=x -F auid>=1000 -F auid!=4294967295 -F key=privileged" >> "$PRIFILE"
