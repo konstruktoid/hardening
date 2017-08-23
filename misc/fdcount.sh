8f074154713187e605c4ea5ac9a220931a8cb65e
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2017-08-23 11:52:34 +0200

    csv
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- misc/fdcount.sh
+++ misc/fdcount.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-echo "username : pid : fd count"
+echo "username, pid, fd count"
 for p in $(awk -F ":" '{print $1}' /etc/passwd); do
   for u in $(ps -C "$p" -o pid=); do
-    fdc="$(find /proc/$u/fd/ | wc -l)"
-    echo "$p : $u : $fdc"
+    fdc="$(find /proc/"$u"/fd/ | wc -l)"
+    echo "$p, $u, $fdc"
   done
 done
