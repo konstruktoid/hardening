746422548a386a4d5d78a0b29af8bdfc3cd86e41
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2017-08-23 11:45:32 +0200

    count fds
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

---
+++ misc/fdcount.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "username : pid : fd count"
+for p in $(awk -F ":" '{print $1}' /etc/passwd); do
+  for u in $(ps -C "$p" -o pid=); do
+    fdc="$(find /proc/$u/fd/ | wc -l)"
+    echo "$p : $u : $fdc"
+  done
+done
