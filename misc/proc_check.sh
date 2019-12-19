645a491b002e20c4d807b1c22e9208b57c509db4
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2019-12-19 09:00:26 +0100

    update proc_check.sh
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- misc/proc_check.sh
+++ misc/proc_check.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-echo "Service|User, NOFILE, NPROC"
-echo "system ulimit, $(ulimit -n | awk '{print $NF}'), $(ulimit -u | awk '{print $NF}')"
+echo "[i] Service|User, NOFILE, NPROC"
+echo "[i] system ulimit: $(ulimit -n | awk '{print $NF}'), $(ulimit -u | awk '{print $NF}')"
+echo
+echo "[i] systemd limits:"
 
-for x in $(systemctl --state=running --state=active | grep -E '.*\.service' | awk '{print $1}'); do 
-	echo "$x, $(systemctl show "$x" | grep -i 'nofile' | sed 's/.*=//g'), $(systemctl show "$x" | grep -i 'nproc' | sed 's/.*=//g')"
-done
+if [ "$1" == "all" ]; then
+  for x in $(systemctl --state=running --state=active | grep -E '.*\.service' | grep -v '@' | awk '{print $1}'); do
+    echo "$x:"
+    systemctl show "$x" | grep -iE 'nofile|nproc'
+    echo
+  done
+fi
 
 while read -r u; do
-	echo "$u, $(systemctl show "user@(id -n $u).service" | grep -i 'nofile' | sed 's/.*=//g'), $(systemctl show "user@(id -n $u).service" | grep -i 'nproc' | sed 's/.*=//g')"
+  echo "$u:"
+  systemctl show "user@(id -n $u).service" | grep -Ei 'nofile|nproc'
+  echo
 done <<< "$(awk -F ':' '{print $1}' /etc/passwd)"
