f20513a31579e2ebab246673c572aa9a2664a6d5
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2021-10-06 10:37:35 +0200

    reorder some passwords, use dos2unix if it exists
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- misc/genPasswordList.sh
+++ misc/genPasswordList.sh
@@ -4,3 +4,7 @@ curl -sSL https://www.ncsc.gov.uk/static-assets/documents/PwnedPasswordsTop100k.
 curl -sSL https://raw.githubusercontent.com/dropbox/zxcvbn/master/data/passwords.txt | awk '{print $1}' > zxcvbn.list
 grep -v '^#' ./ncschabp.list ./zxcvbn.list | sed 's/.*://g' |\
   grep -Ei '^[a-z]|^[0-9]' | sort | uniq > passwords.list
+
+if [ -x "$(which dos2unix)" ]; then
+  dos2unix ./*.list
+fi
