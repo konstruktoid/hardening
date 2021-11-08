4201eb8039a9e56519c3b0ae7d4df762503b46b5
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2021-11-08 10:43:15 +0100

    update passwords with lists from @danielmiessler
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- misc/genPasswordList.sh
+++ misc/genPasswordList.sh
@@ -2,7 +2,11 @@
 
 curl -sSL https://www.ncsc.gov.uk/static-assets/documents/PwnedPasswordsTop100k.txt > ncschabp.list
 curl -sSL https://raw.githubusercontent.com/dropbox/zxcvbn/master/data/passwords.txt | awk '{print $1}' > zxcvbn.list
-grep -v '^#' ./ncschabp.list ./zxcvbn.list | sed 's/.*://g' |\
+curl -sSL https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Leaked-Databases/singles.org.txt > leaked.list
+curl -sSL https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Leaked-Databases/Lizard-Squad.txt >> leaked.list
+curl -sSL https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Leaked-Databases/NordVPN.txt >> leaked.list
+
+grep -v '^#' ./leaked.list ./ncschabp.list ./zxcvbn.list | sed 's/.*://g' |\
   grep -Ei '^[a-z]|^[0-9]' | sort | uniq > passwords.list
 
 if [ -x "$(which dos2unix)" ]; then
