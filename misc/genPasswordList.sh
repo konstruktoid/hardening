39127cee9ab728ddfef74f90d213ea946dede60a
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2022-02-24 22:59:47 +0100

    add konstruktoid/honeypot-passwords url
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- misc/genPasswordList.sh
+++ misc/genPasswordList.sh
@@ -9,6 +9,7 @@ curl -sSL https://raw.githubusercontent.com/dropbox/zxcvbn/master/data/passwords
 curl -sSL https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Leaked-Databases/singles.org.txt > leaked.list
 curl -sSL https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Leaked-Databases/Lizard-Squad.txt >> leaked.list
 curl -sSL https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Leaked-Databases/NordVPN.txt >> leaked.list
+curl -sSL https://raw.githubusercontent.com/konstruktoid/honeypot-passwords/main/cowrie.list > cowrie.list
 
 grep -v '^$' cowrie.list >> "${TMPFILE}"
 
