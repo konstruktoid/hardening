dc7ec4158f457dc753d93abcf1a08e4d588bde99
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2022-12-12 15:37:14 +0100

    bump SCAP content to 0.1.65
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- misc/genOSCAPreport.sh
+++ misc/genOSCAPreport.sh
@@ -3,17 +3,16 @@
 set -u
 set -o pipefail
 
-CONTENT="0.1.64"
-OVAL="5.10"
+CONTENT="0.1.65"
 PROFILE="cis_level2_server"
 REPORT_DATE="$(date +%y%m%d)"
 REPORT_NAME="$(hostname -s)-${PROFILE}-report-${REPORT_DATE}.html"
 
 sudo apt-get -y update
 sudo apt install -y libopenscap8 unzip wget
-wget "https://github.com/ComplianceAsCode/content/releases/download/v${CONTENT}/scap-security-guide-${CONTENT}-oval-${OVAL}.zip"
-unzip "scap-security-guide-${CONTENT}-oval-${OVAL}.zip"
-cd "scap-security-guide-${CONTENT}-oval-${OVAL}" || exit 1
+wget "https://github.com/ComplianceAsCode/content/releases/download/v${CONTENT}/scap-security-guide-${CONTENT}.zip"
+unzip "scap-security-guide-${CONTENT}.zip"
+cd "scap-security-guide-${CONTENT}" || exit 1
 
 if lsb_release -d | grep -qo 'Ubuntu\s20.04'; then
   sudo oscap xccdf eval --profile "xccdf_org.ssgproject.content_profile_${PROFILE}" --results-arf "results-${REPORT_DATE}.xml" --report "${REPORT_NAME}" ./ssg-ubuntu2004-ds-1.2.xml
