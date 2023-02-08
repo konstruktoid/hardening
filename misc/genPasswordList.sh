dd5844e41c8d732c518e4d855a05250b5e0f5bda
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2023-02-08 09:52:24 +0100

    simplify genPasswordList.sh and update password lists
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- misc/genPasswordList.sh
+++ misc/genPasswordList.sh
@@ -12,14 +12,12 @@ curl -sSL https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passw
 curl -sSL https://raw.githubusercontent.com/konstruktoid/honeypot-passwords/main/cowrie.list > cowrie.list
 
 grep -v '^$' cowrie.list >> "${TMPFILE}"
-
-grep -vE '^$|^#' ./leaked.list ./ncschabp.list ./zxcvbn.list | sed 's/.*://g' |\
-  grep -Ei '^[a-z]|^[0-9]' >> "${TMPFILE}"
+grep -hvE '^$' ./*.list | grep -Ei '^[a-z]|^[0-9]' >> "${TMPFILE}"
 
 if [ -x "$(which dos2unix)" ]; then
   dos2unix ./*.list "${TMPFILE}"
 fi
 
-grep -v '^$' "${TMPFILE}" | sort | uniq | strings > passwords.list
+grep -v '^$' "${TMPFILE}" | strings | sort | uniq > passwords.list
 
 rm "${TMPFILE}"
